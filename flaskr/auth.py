94febbefbd0dabeee1bb1de9dd55719181a315f8
Twan Terstappen <32594179+Twan2013@users.noreply.github.com>
2022-12-15 14:55:17 +0100

    Add files via upload

--- flaskr/auth.py
+++ flaskr/auth.py
@@ -1,10 +1,11 @@
-import functools
+import mysql.connector
 
 from flask import (
     Blueprint, g, redirect, render_template, request, session, url_for
 )
 
-bp = Blueprint('auth', __name__, url_prefix='/auth')
+bp = Blueprint('auth', __name__) # url_prefix='/auth' for in the blueprint
+
 
 @bp.route('/login', methods=('GET', 'POST'))
 def login():
@@ -12,14 +13,28 @@ def login():
         name = request.form['name']
         ticketnumber = request.form['ticketnumber']
 
-        session['name'] = name
-        session['ticketnumber'] = ticketnumber
-        return redirect(url_for('test'))
+        connectie = mysql.connector.connect(
+            host="localhost",
+            database="Login",
+            user="user_database",
+            password="Welkom123!"
+        )
+        cursor = connectie.cursor()
+        query = "SELECT name, ticketnumber FROM Login WHERE name = %s and ticketnumber = %s"
+        param = (name, ticketnumber)
+        cursor.execute(query, param)
+        entry = cursor.fetchone()
+        connectie.close()
 
-
-
-
+        # check if something returned
+        if entry:
+            session['name'] = name
+            session['ticketnumber'] = ticketnumber
+            return redirect(url_for('test'))
+        else:
+            session.clear()
     return render_template('login.html')
+
 
 @bp.before_app_request
 def load_logged_in_user():
@@ -30,10 +45,8 @@ def load_logged_in_user():
     # else:
     #     g.usert = 'CHANGME'
 
+
 @bp.route('/logout')
 def logout():
     session.clear()
     return redirect(url_for('main'))
-
-
-
